af777ec7acdf828b99e4486ad5a27780da3e13e1
igor <diegoigor2001@gmail.com>
2022-11-09 09:34:58 -0300

    coriigindo repositorio

--- crud JS/crud/projeto_inicial/admin/service/clienteService.js
+++ crud JS/crud/projeto_inicial/admin/service/clienteService.js
@@ -1,3 +1,21 @@
+const criarNovaLinha = (nome, email) =>{
+    const linhaNovoCliente = document.createElement('tr')
+    const conteudo = `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+        <td>
+    <ul class="tabela__botoes-controle">
+    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                        </ul>
+                    </td> 
+    
+    `
+    linhaNovoCliente.innerHTML = conteudo;
+    return linhaNovoCliente;
+}
+
+
 const http = new XMLHttpRequest()
 
 http.open('GET', 'http://localhost:3000/profile');
